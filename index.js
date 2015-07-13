f534ed0f2c6e58c2cc54efb74e6f87eeed666e22
Scott Corgan <scottcorgan@gmail.com>
2015-07-13 11:53:01 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var path = require('path');
+
 var urlJoin = require('url-join');
 var asArray = require('as-array');
 var _isUrl = require('is-url');
 
 var exports = module.exports = function () {
-  
-  var paths = asArray(arguments)
-    .map(replaceUndefined);
-  
+
+  var paths = asArray(arguments).map(replaceUndefined);
+
   return isUrl(paths[0])
     ? urlJoin.apply(urlJoin, paths)
     : path.join.apply(path, paths);
 };
 
 var isUrl = exports.isUrl = function (url) {
-  
+
   return _isUrl(url)
     || url === 'http://'
     || url === 'https://'
@@ -22,8 +22,8 @@ var isUrl = exports.isUrl = function (url) {
 };
 
 var replaceUndefined = exports.replaceUndefined = function (p, idx, paths) {
-  
-  return  (p == undefined || p == null)
+
+  return  (p === undefined || p === null)
     ? isUrl(paths[0]) ? '/' : path.sep
     : p;
 }
